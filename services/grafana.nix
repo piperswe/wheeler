3e49e4b70d96b2a40d2562dd5a280f6723e3773a
Piper McCorkle <contact@piperswe.me>
2022-11-17 13:05:55 -0600

    Fix Grafana config

--- services/grafana.nix
+++ services/grafana.nix
@@ -2,9 +2,11 @@
 {
   services.grafana = {
     enable = true;
-    domain = "grafana.piperswe.me";
-    port = 8224;
-    addr = "127.0.0.1";
+    settings.server = {
+      domain = "grafana.piperswe.me";
+      http_port = 8224;
+      http_addr = "127.0.0.1";
+    };
   };
   services.nginx.virtualHosts."grafana.piperswe.me" = {
     locations."/" = {
