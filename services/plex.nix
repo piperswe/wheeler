9ed9a5dfef8c2e7c4798842b2abecfd74aaa8642
Piper McCorkle <contact@piperswe.me>
2023-02-09 08:25:52 -0600

    setup plex reverse proxy

--- services/plex.nix
+++ services/plex.nix
@@ -4,5 +4,22 @@
     enable = true;
     openFirewall = true;
   };
+  services.nginx.virtualHosts."plex.piperswe.me" = {
+    locations."/" = {
+      proxyPass = "http://localhost:32400";
+      proxyWebsockets = true;
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $http_x_real_ip;
+        proxy_set_header X-Forwarded_for $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $http_x_forwarded_proto;
+        proxy_set_header Sec-WebSocket-Extensions $http_sec_websocket_extensions;
+        proxy_set_header Sec-WebSocket-Key $http_sec_websocket_key;
+        proxy_set_header Sec-WebSocket-Version $http_sec_websocket_version;
+        proxy_redirect off;
+        proxy_buffering off;
+      '';
+    };
+  };
   users.users.plex.extraGroups = [ "music-library" "video-library" ];
 }
