07427abc4fd1edd7a259bd871fb4f2a080764f30
Piper McCorkle <contact@piperswe.me>
2023-12-12 19:53:31 -0600

    fix permissions

--- services/radarr.nix
+++ services/radarr.nix
@@ -1,5 +1,5 @@
 {
   services.radarr.enable = true;
   systemd.services.radarr.serviceConfig.IOSchedulingPriority = "7";
-  users.users.radarr.extraGroups = [ "media-downloads" ];
+  users.users.radarr.extraGroups = [ "media-downloads" "video-library" ];
 }
